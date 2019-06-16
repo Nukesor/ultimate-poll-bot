2d8e593323d8b2ccb7d55a75282219648d30b753
Arne Beer <arne@twobeer.de>
2019-06-16 05:05:30 +0200

    No special management text

--- pollbot/helper/display/management.py
+++ pollbot/helper/display/management.py
@@ -6,11 +6,4 @@ def get_poll_management_text(session, poll, show_warning=False):
     """Create the management interface for a poll."""
     poll_text = get_poll_text(session, poll, show_warning)
 
-    # Poll is closed, the options are not important any longer
-    if poll.closed:
-        return poll_text
-
-    management_text = 'Manage your poll:\n\n'
-    management_text += poll_text
-
-    return management_text
+    return poll_text
