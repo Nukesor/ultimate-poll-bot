b72dbc0fc8540d47afbdf0dacf489b2941187636
Arne Beer <arne@twobeer.de>
2019-07-13 18:43:23 +0200

    Fix broadcast

--- pollbot/telegram/commands/admin.py
+++ pollbot/telegram/commands/admin.py
@@ -12,7 +12,7 @@ from pollbot.config import config
 @session_wrapper()
 def broadcast(bot, update, session, user):
     """Broadcast a message to all users."""
-    if user.username.lower() != config.telegram.admin.lower():
+    if user.username.lower() != config['telegram']['admin'].lower():
         return 'You are not allowed to do this'
     message = update.message.text.split(' ', 1)[1].strip()
 
@@ -20,20 +20,22 @@ def broadcast(bot, update, session, user):
 
     update.message.chat.send_message(f'Sending broadcast to {len(users)} chats.')
     for user in users:
+        print('sending')
+        print(user.id)
         try:
             bot.send_message(user.id, message, parse_mode='Markdown')
 
         # The chat doesn't exist any longer, delete it
         except BadRequest as e:
             if e.message == 'Chat not found': # noqa
-                continue
+                pass
 
         # We are not allowed to contact this user.
         except Unauthorized:
-            continue
+            pass
 
         # Sleep one second to not trigger flood prevention
-        time.sleep(2)
+        time.sleep(1)
 
     update.message.chat.send_message('All messages sent')
 
