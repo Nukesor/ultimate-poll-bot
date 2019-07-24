097bef1bfa377abde1b2781ce7d934db17cd00ac
Arne Beer <arne@twobeer.de>
2019-07-24 11:57:34 +0200

    Fix doodle sorting key mapping

--- pollbot/helper/display/__init__.py
+++ pollbot/helper/display/__init__.py
@@ -15,8 +15,13 @@ def get_sorted_votes(poll, votes):
         return vote.user.name
 
     def get_doodle_int(vote):
-        """Get the name of user to sort votes."""
-        return VoteResultType[vote.type].value
+        """Get the sorting key for the vote type."""
+        mapping = {
+            'yes': 1,
+            'maybe': 2,
+            'no': 3,
+        }
+        return mapping[vote.type]
 
     if poll.poll_type == PollType.doodle.name:
         votes.sort(key=get_doodle_int)
