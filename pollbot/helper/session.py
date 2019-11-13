c6ee6ef0201995ac667cd585be1041b92e5ba8d0
Arne Beer <arne@twobeer.de>
2019-11-14 00:34:43 +0100

    Ignore yet another error

--- pollbot/helper/session.py
+++ pollbot/helper/session.py
@@ -162,6 +162,8 @@ def ignore_exception(exception):
             return True
         if exception.message == 'Forbidden: user is deactivated':
             return True
+        if exception.message == 'Forbidden: bot was kicked from the group chat':
+            return True
 
     if isinstance(exception, TimedOut):
         return True
