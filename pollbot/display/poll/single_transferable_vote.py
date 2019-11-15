7690db846e45e9e0d816a8f4cd508da1e53c6a96
Rafael Epplee <raffomania@gmail.com>
2019-11-15 16:46:14 +0100

    make stv algorithm work for now

--- pollbot/display/poll/single_transferable_vote.py
+++ pollbot/display/poll/single_transferable_vote.py
@@ -1,4 +1,4 @@
-from pollbot.models import Vote, User
+from pollbot.models import Vote, User, PollOption
 from sqlalchemy import func
 from collections import Counter
 
@@ -8,14 +8,39 @@ def get_stv_result(session, poll):
         .filter(Vote.poll_id == poll.id) \
         .all()
 
-    print(get_ranked_options(session, poll, poll.options, users))
-    return ['Tap "vote" to rank the options by preference']
+    option_ids = [option.id for option in poll.options]
+    print('init ids',option_ids)
+    for _ in range(len(option_ids) - 1):
+        ranked_options = get_ranked_options(session, poll, option_ids, users)
+        print('ranked', ranked_options)
+        last_id, last_rank = ranked_options[-1]
+        options_with_same_rank = [
+            id for id, rank in ranked_options
+            if rank == last_rank and id != last_id
+        ]
 
-def get_ranked_options(session, poll, options, users):
-    option_ids = [option.id for option in options]
-    option_votes = Counter()
+        if (len(options_with_same_rank) > 0):
+            names = ','.join([
+                session.query(PollOption).get(id).name
+                for id in options_with_same_rank + [last_id]
+            ])
+            return [f'There was a tie between {names}. Close the poll to resolve it using a random choice.']
+
+        option_ids = [
+            id for id in option_ids
+            if id != last_id
+        ]
+
+    assert len(option_ids) == 1
+    winning_option = session.query(PollOption).get(option_ids[0])
+    return [f'Winner: {winning_option.name}']
+    # return ['Tap "vote" to rank the options by preference']
+
+def get_ranked_options(session, poll, option_ids, users):
+    option_votes = Counter({id: 0 for id in option_ids})
     for user in users:
         for vote in sorted(user.votes, key=lambda vote: vote.priority):
+            print('prio', vote.poll_option_id, vote.priority)
             if vote.poll_option_id in option_ids:
                 option_votes[vote.poll_option_id] += 1
                 break
