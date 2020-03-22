20f79020e58efef2868ba9d46b07839e31b2f1b6
Arne Beer <contact@arne.beer>
2020-03-22 11:39:50 +0100

    Handle can't access chat exception

--- pollbot/helper/update.py
+++ pollbot/helper/update.py
@@ -118,7 +118,8 @@ def send_updates(session, bot, poll, show_warning=False):
             if e.message.startswith('Message_id_invalid') or \
                    e.message.startswith("Message can't be edited") or \
                    e.message.startswith("Message to edit not found") or \
-                   e.message.startswith("Chat not found"):
+                   e.message.startswith("Chat not found") or \
+                   e.message.startswith("Can't access the chat"):
                 session.delete(reference)
                 session.commit()
             elif e.message.startswith('Message is not modified'):
