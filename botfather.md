f428cbf6f15c2c89e4ed1993d9c29f1e3d34816c
Joscha Götzer <joscha.goetzer@teamviewer.com>
2020-05-29 17:41:15 +0200

    - Added command line interface to `initdb.py` (and additional dev dependency "Typer", which is a wrapper around "click") - Moved botfather commands out of README into a separate file - Added docker/docker-compose.yml for easy setup of development database - Updated README

---
+++ botfather.md
@@ -0,0 +1,12 @@
+## Botfather Commands
+
+    start - Start the bot
+    stop - Stop the bot
+    delete_me - Remove me from the bot. Forever
+    settings - Open the user settings menu
+    create - Create a new poll
+    list - List all active polls and manage them
+    list_closed - List all closed polls and manage them
+    notify - Activate notifications in external chats
+    help - Show the help text
+    donations - Get me a coffee
--- initdb.py
+++ initdb.py
@@ -1,15 +1,53 @@
 #!/bin/env python
-"""Drop and create a new database with schema."""
+"""Create a new database with schema."""
+from contextlib import contextmanager
 
-from sqlalchemy_utils.functions import database_exists, create_database
+import typer
+from sqlalchemy_utils.functions import database_exists, create_database, drop_database
+
 from pollbot.db import engine, base
 from pollbot.models import *  # noqa
 
-db_url = engine.url
-if not database_exists(db_url):
-    create_database(db_url)
+
+def initialize_database(exist_ok: bool = False, drop_existing: bool = False):
+    db_url = engine.url
+    typer.echo(f"Using database at {db_url}")
+
+    if database_exists(db_url):
+        if drop_existing:
+            with wrap_echo("Dropping database"):
+                drop_database(db_url)
+        elif not exist_ok:
+            typer.echo(
+                f"Database already exists, aborting.\n"
+                f"Use --exist-ok if you are sure the database is uninitialized and contains no data.\n"
+                f"Use --drop-existing if you want to recreate it.",
+                err=True,
+            )
+            return
+
+    with wrap_echo("Creating database"):
+        create_database(db_url)
+        pass
 
     with engine.connect() as con:
-        con.execute("CREATE EXTENSION IF NOT EXISTS pgcrypto;")
+        with wrap_echo("Installing pgcrypto extension"):
+            con.execute("CREATE EXTENSION IF NOT EXISTS pgcrypto;")
+            pass
 
-    base.metadata.create_all()
+    with wrap_echo("Creating metadata"):
+        base.metadata.create_all()
+        pass
+
+    typer.echo("Database initialization complete.")
+
+
+@contextmanager
+def wrap_echo(msg: str):
+    typer.echo(f"{msg}... ", nl=False)
+    yield
+    typer.echo("done.")
+
+
+if __name__ == "__main__":
+    typer.run(initialize_database)
