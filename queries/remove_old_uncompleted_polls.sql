8dfa7d3152aeb876fd648314bf64a64adb6ff314
Arne Beer <contact@arne.beer>
2021-09-26 01:41:23 +0200

    Remove unnecessary files

--- queries/remove_old_uncompleted_polls.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM "poll" as p1
-    WHERE created = False
-    AND EXISTS (
-        SELECT * from poll as p2
-            WHERE p2.created_at > p1.created_at
-            AND p1.user_id = p2.user_id
-        )
--- queries/vote_with_poll.sql
+++
@@ -1,7 +0,0 @@
-SELECT p.id, p.name, p.poll_type, o.name, v.id, v.type
-    FROM poll AS p
-    JOIN vote AS v ON poll.id = vote.poll_id
-    JOIN option AS o ON vote.option_id = option.id
-WHERE p.created IS TRUE ORDER BY p.id, o.id DESC limit 200
-
-# WHERE p.id = :number;
